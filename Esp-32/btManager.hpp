1541c8cedba41776d00302ad3c1446826575d1c1
Chitic David <chiticdavid05@gmail.com>
2025-04-28 23:17:00 +0300

    Esp-32 update
    am adaugat si bluetooth pt esp-32

---
+++ Esp-32/btManager.hpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <cstring>
+
+
+class btManager{
+  public:
+    btManager(HardwareSerial& serial) : serial(serial){
+      serial.begin(9600);
+      serialBt.begin("ESP-32");
+      serial.println("Bluetooth ready. Waiting for data...");
+    } 
+    void Send(char* message){
+      serialBt.println(message);
+    }
+    
+    void Receieve() {
+      if(serialBt.available()) {
+        int character = serialBt.read();
+
+        if (character != '\n' && newLineIndex < 127) { // Check for newline and buffer overflow
+          newLine[newLineIndex++] = (char)character;
+          newLine[newLineIndex] = '\0'; // Null-terminate the string
+        } else if (character == '\n') {
+
+          strcpy(currentLine, newLine);
+          processMessage(currentLine);
+          newLineIndex = 0; // Reset index
+          newLine[0] = '\0'; // Clear the string
+
+        }
+      }
+    }
+    
+  private:
+    BluetoothSerial serialBt;
+    HardwareSerial serial;
+    char currentLine[128] = "";
+    char newLine[128] = "";
+    int newLineIndex = 0;
+
+    void processMessage(const char* message)
+    {
+
+    }
+    
+};
